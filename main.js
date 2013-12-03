86c9da9d8f753d64382c22b437affb5032aefcb4
Christine McClure <camcclure@gmail.com>
2013-12-03 15:43:42 -0600

    successfully populating random book from file

--- main.js
+++ main.js
@@ -34,44 +34,55 @@
         }
 
         function loadRandomBook(){
-
+            var debug=false;
             var truncLen = 200;
-            var numBooks = Books.length;
-            var randomNum=Math.floor(Math.random()*(numBooks))
-            var book = Books[randomNum];
-            var bookURL = 'https://vufind.carli.illinois.edu/vf-iit/Search/Home?lookfor=' + book.isbn + '&type=all&start_over=1&submit=Find&search=new';
-            if (!book.description)
-              var randomBookDescText = "No description available.";
-            else
-              var randomBookDescText = book.description;
 
-            if (randomBookDescText.length > truncLen) {
-               var descTrunc = randomBookDescText.substring(0,truncLen) + "...";
-            }
+            $.getJSON("leisureBooks.json", function(json) {
+                var numBooks = json.leisureBooks.length;
+                if (debug) alert('There are '+ numBooks + ' books in the file.');
+                var randomNum=0;
+                randomNum=Math.floor(Math.random()*(numBooks));
+                if (debug) alert('number is '+ randomNum);
+                var book = json.leisureBooks[randomNum];
+                if (debug) alert('Book is '+ book.title);
+
+                var bookURL = 'https://vufind.carli.illinois.edu/vf-iit/Search/Home?lookfor=' + book.isbn + '&type=all&start_over=1&submit=Find&search=new';
+                if (!book.summary)
+                  var randomBookDescText = "No description available.";
+                else
+                  var randomBookDescText = book.summary;
+
+                if (randomBookDescText.length > truncLen) {
+                   var descTrunc = randomBookDescText.substring(0,truncLen) + "...";
+                }
+
+                emptyContainer();
+
+                $('#bookTitle').append($('<p>').html(book.title));
+                $('#bookAuthor').append($('<p>').html(book.author));
+
+                if (descTrunc){
+                  $('#bookDescText').html('<p>'+descTrunc+'<button id=\'showFull\' class=\"btn btn-info btn-xs\">Show full description</button></p>');
+                }
+                else {
+                  $('#bookDescText').append($('<p>').html(randomBookDescText));
+                }
+
+                $('#showOPACbtn').html('<button class="btn btn-success">Interesting. Is it checked out?</a></button>');
+                $('#newBookBtn').html( '<button class="btn btn-danger">No, thanks. Show me another book</button>');
+
+                $( "#showFull" ).bind( "click", function( ) { // add click event for dynamically-created element
+                   $('#bookDescText').empty();
+                   $('#bookDescText').append($('<p>').html(randomBookDescText));
+                });
+                $( "#showOPACbtn" ).bind( "click", function( ) { // add click event for dynamically-created element
+                  window.location.href=bookURL;
+                });
 
 
-            emptyContainer();
-
-            $('#bookTitle').append($('<p>').html(book.title));
-            $('#bookAuthor').append($('<p>').html(book.author));
-
-            if (descTrunc){
-              $('#bookDescText').html('<p>'+descTrunc+'<button id=\'showFull\' class=\"btn btn-info btn-xs\">Show full description</button></p>');
-            }
-            else {
-              $('#bookDescText').append($('<p>').html(randomBookDescText));
-            }
-
-            $('#showOPACbtn').html('<button class="btn btn-success">Interesting. Is it checked out?</a></button>');
-            $('#newBookBtn').html( '<button class="btn btn-danger">No, thanks. Show me another book</button>');
-
-            $( "#showFull" ).bind( "click", function( ) { // add click event for dynamically-created element
-               $('#bookDescText').empty();
-               $('#bookDescText').append($('<p>').html(randomBookDescText));
             });
-            $( "#showOPACbtn" ).bind( "click", function( ) { // add click event for dynamically-created element
-              window.location.href=bookURL;
-            });
+
+
 
 
         }
