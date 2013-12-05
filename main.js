301959a5491b2b877a1b300b1f95bc424cc74849
Christine McClure <camcclure@gmail.com>
2013-12-05 10:58:31 -0600

    code cleanup

--- main.js
+++ main.js
@@ -56,89 +56,65 @@ function checkIfInArray(arr, item){
   return flag;
 }
 
-        function generateRandomNumbers(howMany, numBooks){
-            var debug=false;
-                  if (debug) numBooks = 5; // force more duplicates
-            var numArr= new Array; //make sure to try with just 1
-            var randomNum;
-            var flag;
-            for (var i=0; i<howMany; i++){
-              flag=true;
-              while (flag==true){
-                randomNum=Math.floor(Math.random()*(numBooks));
-                flag = checkIfInArray(numArr, randomNum);
-                if (flag ==false){
-                  numArr[i]=randomNum;
-                }
-              }
+function generateRandomNumbers(howMany, numBooks){
+    var debug=false;
+          if (debug) numBooks = 5; // force more duplicates
+    var numArr= new Array; //make sure to try with just 1
+    var randomNum;
+    var flag;
+    for (var i=0; i<howMany; i++){
+      flag=true;
+      while (flag==true){
+        randomNum=Math.floor(Math.random()*(numBooks));
+        flag = checkIfInArray(numArr, randomNum);
+        if (flag ==false){
+          numArr[i]=randomNum;
+        }
+      }
+    }
+    if (debug) alert(JSON.stringify(numArr));
+    return numArr;
+}
+
+  function loadRandomBook(){
+      var debug=false;
+      var debug2=false;
+      var truncLen = 200;
+      var bookSummaryTxt;
+
+      $.getJSON(dataFile, function(json) {
+          var numBooks = json.leisureBooks.length;
+          Books = json.leisureBooks;
+          var numArr=generateRandomNumbers(howMany, numBooks);
+            for (var i=0; i<=numArr.length; i++){
+                var thisBook=Books[numArr[i]];
+                var bookURL = 'https://vufind.carli.illinois.edu/vf-iit/Search/Home?lookfor=' + thisBook.isbn + '&type=all&start_over=1&submit=Find&search=new';
+                if (!thisBook.summary)
+                    bookSummaryTxt = "No description available.";
+                else
+                    bookSummaryTxt = thisBook.summary;
+//                  if (bookSummaryTxt.length > truncLen) {
+//                     var descTrunc = bookSummaryTxt.substring(0,truncLen) + "...";
+//                  }                   
+//                if (descTrunc){
+//                  row += '<td>' + descTrunc + '<button id=\'showFull\' class=\"\">Show full description</button>' + '</td>';
+//                }
+//                else {
+//                  row += '<td>' + bookSummaryTxt + '</td>';
+//                }                      
+
+                  var row = '<tr id=\"'+i+'\">';
+                  row += '<td>' + thisBook.title + '</td>';
+                  row += '<td>' + thisBook.author + '</td>';
+                  row += '<td>' + bookSummaryTxt + '</td>'
+                  row += '<td><a  target=\"_blank\" href=\"'+ bookURL +'<p class=\"button\">Is it checked out?</p></a></td>'
+                  row += '</tr>';
+
+                  $('#bookList').append(row);
             }
-            if (debug) alert(JSON.stringify(numArr));
-            return numArr;
-        }
-        
-        function loadRandomBook(){
-            var debug=false;
-            var debug2=false;
-            var truncLen = 200;
-            var bookSummaryTxt;
-
-            $.getJSON(dataFile, function(json) {
-                var numBooks = json.leisureBooks.length;
-//                if (debug) alert('There are '+ numBooks + ' books in the file.');
-                Books = json.leisureBooks;
-                //if (debug) alert('number is '+ randomNum);
-//                if (debug) alert('Book is '+ Books[randomNum].title);
-                var numArr=generateRandomNumbers(howMany, numBooks);
-//                if (debug2) console.log(JSON.stringify(numArr));
-                  for (var i=0; i<=numArr.length; i++){
-                      var thisBook=Books[numArr[i]];
-//                      alert(JSON.stringify(thisBook));
-                      var bookURL = 'https://vufind.carli.illinois.edu/vf-iit/Search/Home?lookfor=' + thisBook.isbn + '&type=all&start_over=1&submit=Find&search=new';
-                      if (!thisBook.summary)
-                          bookSummaryTxt = "No description available.";
-                      else
-                          bookSummaryTxt = thisBook.summary;
-                      if (bookSummaryTxt.length > truncLen) {
-                         var descTrunc = bookSummaryTxt.substring(0,truncLen) + "...";
-                      }
-                   
-                if (descTrunc){
-                  row += '<td>' + descTrunc + '<button id=\'showFull\' class=\"btn btn-info btn-xs\">Show full description</button>' + '</td>';
-                }
-                else {
-                  row += '<td>' + bookSummaryTxt + '</td>';
-                }                      
-
-                        var row = '<tr id=\"'+i+'\">';
-                        row += '<td>' + thisBook.title + '</td>';
-                        row += '<td>' + thisBook.author + '</td>';
-                        row += '<td>' + bookSummaryTxt + '</td>'
-                        row += '<td><a  target=\"_blank\" href=\"'+ bookURL +'<p class=\"button\">Checked out?</p></a></td>'
-                        row += '</tr>';
-                        
-                        $('#bookList').append(row);
-                
-                  }
-//                $('#1').append($('<p>').html(book.title));
-//                $('#bookAuthor').append($('<p>').html(book.author));
-//
-
-//
-//                $('#showOPACbtn').html('<button class="btn btn-success">Checked out?</a></button>');
-//                $('#newBookBtn').html( '<button class="btn btn-danger">No, thanks. Show me another book</button>');
-//
-//                $( "#showFull" ).bind( "click", function( ) { // add click event for dynamically-created element
-//                   $('#bookDescText').empty();
-//                   $('#bookDescText').append($('<p>').html(randomBookDescText));
-//                });
-//                $( "#showOPACbtn" ).bind( "click", function( ) { // add click event for dynamically-created element
-//                  window.open(bookURL,'_blank');
-//                });
-//
-
-            });
 
 
+      });
 
 
-        }
+}
