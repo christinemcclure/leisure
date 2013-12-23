faed144afcd6337202f8c0dd74238a3a6be8e57e
Christine McClure <camcclure@gmail.com>
2013-12-23 10:29:48 -0600

    giving up on this track. moving to event delegation

--- main.js
+++ main.js
@@ -54,9 +54,11 @@ function loadRandomBooks(){
         var numBooks = json.leisureBooks.length;
         Books = json.leisureBooks;
         var numArr=generateRandomNumbers(howMany, numBooks);
+
           for (var i=0; i<numArr.length; i++){
             var setDesc=false;
             var thisBook=Books[numArr[i]];
+            var bookDesc = [];
             var bookURL = 'https://vufind.carli.illinois.edu/vf-iit/Search/Home?lookfor=' + thisBook.isbn + '&type=all&start_over=1&submit=Find&search=new';
             if (!thisBook.title) // unlikely, but handle anyway
                 thisBook.author="[untitled]";
@@ -67,29 +69,45 @@ function loadRandomBooks(){
             else
                 bookSummaryTxt = thisBook.summary;
 
-            var row = '<tr id=\"'+i+'\">';
+            var row = '<tr id=\"row'+i+'\">';
             row += '<td class=\"title\">' + thisBook.title +'</td>';
             row += '<td>' + thisBook.author + '</td>';
 
             // handle summary
             if (bookSummaryTxt.length > truncLen) {
               bookSummaryTxt = bookSummaryTxt.substring(0,truncLen) + "..."; 
-              console.log('i is '+i+' and title is "'+ thisBook.title + '" and summary is \n' + bookSummaryTxt);
-                var len = 1;
+              console.log('TRUNC - i is '+i+' and title is "'+ thisBook.title + '" and summary is \n' + bookSummaryTxt);
 //              truncArr.push(thisBook.summary); // store full desc in array
 //              console.log (' len is '+len);
 //              console.log ('full desc for \"' + thisBook.title + '\" is: '+ truncArr[len]);
 
-              row += '<td id=\"cell'+i+'\" class=\'desc\'>' + bookSummaryTxt + ' <a id=\'moreLink' + len + '\'>more</a>' + '</td>'; // create more link
+//              row += '<td id=\"cell'+i+'\" class=\'desc\'>' + bookSummaryTxt + ' <a id=\'moreLink' + len + '\'>more</a>' + '</td>'; // create more link
+             bookDesc[i] = thisBook.summary;
+             var moreLinkID = 'moreLink'+i.toString();
+             console.log('*** '+moreLinkID);
+              row += '<td id=\"cell'+i+'\" class=\'desc\'>' + bookSummaryTxt + ' <a id=\'' + moreLinkID + '\'>more</a>' + '</td>'; // create more link
             }
             else {          
+              bookDesc[i] = 0;
               row += '<td class=\'desc\'>' + bookSummaryTxt + '</td>';
             }  
 
             row += '<td><p class=\"button\"><a target=\"_blank\"href=\"'+ bookURL +'\">Checked out?</a></p></td>';
             row += '</tr>';
             
-            $('#bookList').append(row);        
+            $('#bookList').append(row); 
+//            $( "#bookList td#cell"+i+' #moreLink'+i ).on( "click", function() {
+//              alert('i is '+i+' and title is "'+ thisBook.title);
+//            });
+
+            $('#moreLink'+i.toString()).click(function() { 
+              alert('i is '+i + thisBook.summary);
+            });
+
+//            $('#row'+i).on('click', '#'+moreLinkID, function(){
+//                 alert(JSON.stringify(bookDesc[i])); 
+//            });
+
       }// end for
 
 
